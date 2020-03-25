8b6f261e78e544145f9f72b0c1456e97b07f5ea6
Doomky <james.richet.pro@gmail.com>
2020-03-25 15:05:15 +0100

    [World Generation] first version of the world generation using perlin noise to randomize height

---
+++ Assets/Scripts/WorldGeneration/BlockDestroyer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDestroyer : MonoBehaviour
+{
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Collision stay");
+        Block block = collision.gameObject.GetComponent<Block>();
+        if (block)
+            GameObject.Destroy(block.gameObject);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision enter");
+        Block block = collision.gameObject.GetComponent<Block>();
+        if (block)
+            GameObject.Destroy(block.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger enter");
+        Block block = other.gameObject.GetComponent<Block>();
+        if (block)
+            GameObject.Destroy(block.gameObject);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Trigger stay");
+        Block block = other.gameObject.GetComponent<Block>();
+        if (block)
+            GameObject.Destroy(block.gameObject);
+    }
+}
---
+++ Assets/Scripts/WorldGeneration/BlockGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGenerator : MonoBehaviour
+{
+    [SerializeField] protected float lastPositionX;
+    [SerializeField] protected float blocksWidth = 1;
+    [SerializeField] protected float perlinNoiseScale = 1;
+    [SerializeField] protected List<GameObject> blocksPrefabs = new List<GameObject>();
+
+    private void Start()
+    {
+        lastPositionX = transform.position.x;
+    }
+
+    private void FixedUpdate()
+    {
+        while (lastPositionX + blocksWidth <= transform.position.x)
+        {
+
+            GameObject blockPrefab = blocksPrefabs[UnityEngine.Random.Range(0, blocksPrefabs.Count)];
+            float noise = Mathf.PerlinNoise(perlinNoiseScale * lastPositionX, 0);
+            if (noise < 0.2f)
+            {
+                // nothing its a cliff
+            }
+            else
+            {
+                float height = Mathf.RoundToInt(noise * 10);
+                for (int i = 0; i < height; i++)
+                    GameObject.Instantiate(blockPrefab, new Vector3(lastPositionX, -0.5f + i, 0), Quaternion.identity);
+            }
+            lastPositionX += blocksWidth;
+        }
+    }
+}
